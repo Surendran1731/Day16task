ae699026d3608cde0023defa75f29f1eedb8c3d0
Surendran1731 <143333240+Surendran1731@users.noreply.github.com>
2023-12-31 12:40:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+  document.getElementById("number").innerHTML="10";
+  setTimeout(() => {
+    document.getElementById("number").innerHTML="9";
+    setTimeout(() => {
+      document.getElementById("number").innerHTML="8";
+      setTimeout(() => {
+        document.getElementById("number").innerHTML="7";
+        setTimeout(() => {
+          document.getElementById("number").innerHTML="6";
+          setTimeout(() => {
+            document.getElementById("number").innerHTML="5";
+            setTimeout(() => {
+              document.getElementById("number").innerHTML="4";
+              setTimeout(() => {
+                document.getElementById("number").innerHTML="3";
+                setTimeout(() => {
+                  document.getElementById("number").innerHTML="2";
+                  setTimeout(() => {
+                    document.getElementById("number").innerHTML="1";
+                    setTimeout(() => {
+                      document.getElementById("number").innerHTML= "Happy Independance Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+
+ 
